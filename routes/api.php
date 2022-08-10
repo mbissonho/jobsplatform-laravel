feb5bda3c4865608ac21d4c6205f0c58236fe222
Mateus Bissonho <mateusabissonho@gmail.com>
2022-08-10 18:45:09 -0300

    Improve api routes definition

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,9 @@
 <?php
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\JobController;
 use App\Http\Controllers\Api\SkillController;
-use App\Http\Controllers\Api\AuthController;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,19 +18,19 @@ use App\Http\Controllers\Api\AuthController;
 
 Route::name('api.')->group(function (){
 
-    Route::name('jobs.')->group(function (){
-        Route::get('jobs', [JobController::class, 'index'])->name('search');
-        Route::get('jobs/{job}', [JobController::class, 'show'])->name('get-by-id');
-        Route::post('jobs/{job}/applications', [JobController::class, 'applyCandidateTo'])->name('candidate.apply-to-job');
+    Route::group(['prefix' => 'jobs', 'as' => 'jobs.'], function (){
+        Route::get('/', [JobController::class, 'index'])->name('search');
+        Route::get('/{job}', [JobController::class, 'show'])->name('get-by-id');
+        Route::post('/{job}/applications', [JobController::class, 'applyCandidateTo'])->name('candidate.apply-to-job');
     });
 
-    Route::name('skills.')->group(function (){
-        Route::get('skills', [SkillController::class, 'index'])->name('all');
+    Route::group(['prefix' => 'skills', 'as' => 'skills.'], function (){
+        Route::get('/', [SkillController::class, 'index'])->name('all');
     });
 
-    Route::name('auth.')->group(function (){
-        Route::post('users', [AuthController::class, 'register'])->name('candidate.register');
-        Route::post('users/login', [AuthController::class, 'login'])->name('candidate.login');
+    Route::group(['prefix' => 'auth/users', 'as' => 'auth.'], function (){
+        Route::post('/', [AuthController::class, 'register'])->name('candidate.register');
+        Route::post('/login', [AuthController::class, 'login'])->name('candidate.login');
     });
 
 });
